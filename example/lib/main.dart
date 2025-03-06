7f99bf88058b268d95327d34679e30c92b1552cc
Dmitry Poyarkov <14233263+Strash@users.noreply.github.com>
2025-03-06 17:53:34 +0300

    update: example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -12,7 +12,8 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var isLight = MediaQuery.platformBrightnessOf(context) == Brightness.light;
+    final isLight =
+        MediaQuery.platformBrightnessOf(context) == Brightness.light;
     const bgLigth = Color(0xFFFFFFFF);
     const bgDark = Color(0xFF050505);
 
@@ -36,63 +37,158 @@ class MyWidget extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<MyWidget> {
-  final _rng = Random();
-  int _iValue = 0;
+  late final loc = MaterialLocalizations.of(context);
+  final rng = Random();
+  String text = "";
+  int value = 0;
+  int count = 1;
 
-  void _randomize() {
+  String randomString(int length) {
+    if (length <= 0) throw Exception("Length must be greater then 0");
+    final len = (length * 0.5).ceil();
+    final bytes = List<int>.generate(len, (i) => rng.nextInt(256));
+    return bytes.map((byte) => byte.toRadixString(16).padLeft(2, "0")).join();
+  }
+
+  void randomize() {
     setState(() {
-      _iValue += _rng.nextInt(150) + 1;
+      text = randomString(max(1, rng.nextInt(10)));
+      value = rng.nextInt(5000);
+      count++;
     });
   }
 
-  String get _description {
-    var loc = MaterialLocalizations.of(context);
-    return loc.formatDecimal(_iValue);
+  String get valueDescription {
+    return loc.formatDecimal(value);
   }
 
   @override
   void initState() {
     super.initState();
-    _randomize();
+    randomize();
   }
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    var isLight = MediaQuery.platformBrightnessOf(context) == Brightness.light;
-    var style = theme.textTheme.displayMedium?.copyWith(
+    final isLight =
+        MediaQuery.platformBrightnessOf(context) == Brightness.light;
+    final style = theme.textTheme.displaySmall?.copyWith(
       fontWeight: FontWeight.w600,
       color: isLight ? const Color(0xFF000000) : const Color(0xFFCCCCED),
     );
-
-    final description = _description;
+    final subStyle = theme.textTheme.bodyLarge?.copyWith(
+      color: isLight ? const Color(0xAF000000) : const Color(0xAFCCCCED),
+      height: 1.25,
+    );
 
     return Column(
       mainAxisSize: MainAxisSize.min,
       crossAxisAlignment: CrossAxisAlignment.center,
       spacing: 20.0,
       children: [
-        NumericText(
-          "\$$description\nStart",
-          duration: Durations.short4,
-          style: style,
+        Column(
+          children: [
+            NumericText(
+              text,
+              duration: Durations.medium1,
+              textAlign: TextAlign.center,
+              style: style,
+            ),
+            Text(
+              "Some random text",
+              textAlign: TextAlign.center,
+              style: subStyle,
+            ),
+          ],
+        ),
+        const SizedBox(height: 20.0),
+
+        Align(
+          alignment: Alignment.centerLeft,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              NumericText("\$$valueDescription", style: style),
+              Text(
+                "Duration Default\nTextAlign Start",
+                textAlign: TextAlign.start,
+                style: subStyle,
+              ),
+            ],
+          ),
         ),
 
-        NumericText(
-          "\$$description\nCenter",
-          duration: Durations.medium4,
-          textAlign: TextAlign.center,
-          style: style,
+        Column(
+          children: [
+            NumericText(
+              "\$$valueDescription",
+              duration: Durations.medium1,
+              textAlign: TextAlign.center,
+              style: style,
+            ),
+            Text(
+              "Duration Medium1\nTextAlign Center",
+              textAlign: TextAlign.center,
+              style: subStyle,
+            ),
+          ],
         ),
 
-        NumericText(
-          "\$$description\nEnd",
-          duration: Durations.long4,
-          textAlign: TextAlign.end,
-          style: style,
+        Align(
+          alignment: Alignment.centerRight,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              NumericText(
+                "\$$valueDescription",
+                duration: Durations.long1,
+                textAlign: TextAlign.end,
+                style: style,
+              ),
+              Text(
+                "Duration Long1\nTextAlign End",
+                textAlign: TextAlign.end,
+                style: subStyle,
+              ),
+            ],
+          ),
         ),
 
-        FilledButton(onPressed: _randomize, child: Text("Randomize")),
+        Align(
+          alignment: Alignment.center,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              TweenAnimationBuilder(
+                duration: Durations.long4,
+                curve: Curves.fastEaseInToSlowEaseOut,
+                tween: IntTween(begin: 0, end: value),
+                builder: (context, v, child) {
+                  return NumericText("\$${loc.formatDecimal(v)}", style: style);
+                },
+              ),
+              Text(
+                "Animated text",
+                textAlign: TextAlign.start,
+                style: subStyle,
+              ),
+            ],
+          ),
+        ),
+
+        Column(
+          spacing: 5.0,
+          children: [
+            FilledButton(onPressed: randomize, child: Text("Randomize")),
+            NumericText(
+              "Randomized\n$count times",
+              duration: Durations.medium2,
+              textAlign: TextAlign.center,
+              style: subStyle,
+            ),
+          ],
+        ),
       ],
     );
   }
